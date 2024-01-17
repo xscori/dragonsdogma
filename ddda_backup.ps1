3948b978e1fd9c7a7f2b621187cfa4df418fbcef
xscori <156870708+xscori@users.noreply.github.com>
2024-01-16 22:37:08 -0500

    Update ddda_backup.ps1

--- ddda_backup.ps1
+++ ddda_backup.ps1
@@ -1,16 +1,24 @@
 # delete existing watcher events
 Get-EventSubscriber -force | unregister-event -force
 
+# REPLACE: For me save file resides here C:\Program Files (x86)\Steam\userdata\342408821\367500\Remote\DDDA.sav
+# so my steam user id is 342408821
+$uid = 342408821
+
+# REPLACE: note this is where I have the game executable, change it for your installation
+$exe = "G:\games\steam\steamapps\common\DDDA\dda.exe"
+
 # start ddda if it is not already running
 if (-not (get-process ddda)) {
-		"launching dda"
-		# note this is where I have the game executable, change it for your installation
-		start-process G:\games\steam\steamapps\common\DDDA\dda.exe
+		"launching ddda"
+		  		
+		start-process $exe
 }
 
 # setup watcher
 $watcher = New-Object System.IO.FileSystemWatcher
-$watcher.Path = 'C:\Program Files (x86)\Steam\userdata\342408821\367500\remote'
+
+$watcher.Path = "C:\Program Files (x86)\Steam\userdata\$uid\367500\remote"
 $watcher.EnableRaisingEvents = $true
 
 $action =
@@ -18,18 +26,17 @@ $action =
 		$path = $event.SourceEventArgs.FullPath
 		$changetype = $event.SourceEventArgs.ChangeType
 		Write-Host "$path was $changetype at $(get-date)"
-		
-		# number after userdata is different for each user, find your .sav file and replace the line
-		$dd_save_file = "C:\Program Files (x86)\Steam\userdata\342408821\367500\Remote\DDDA.sav"
+				
+		$dd_save_file = "C:\Program Files (x86)\Steam\userdata\$uid\367500\Remote\DDDA.sav"
 
 		# when was it last saved
 		$dd_save_file_date = (gci $dd_save_file).LastWriteTime
 
 		# put the backup folder next to "Remote" folder
-		$dd_backup_folder="C:\Program Files (x86)\Steam\userdata\342408821\367500\Backup"
+		$dd_backup_folder="C:\Program Files (x86)\Steam\userdata\$uid\367500\Backup"
 
 		# find the latest backed up save file & its date
-		$last_backup = (gci "C:\Program Files (x86)\Steam\userdata\342408821\367500\Backup" |Sort-Object LastWriteTime)[-1]
+		$last_backup = (gci "C:\Program Files (x86)\Steam\userdata\$uid\367500\Backup" |Sort-Object LastWriteTime)[-1]
 		$last_backup_date = $last_backup.LastWriteTime
 
 		if ($dd_save_file_date -ne $last_backup_date) {
